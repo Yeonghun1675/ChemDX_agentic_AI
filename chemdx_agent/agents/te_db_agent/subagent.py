ef6d24927b7ec43294e512c4aa7d459346b2c29e
aairehtuele <aairehtuele@gmail.com>
2025-08-20 07:05:59 +0900

    update te agent
    update

--- chemdx_agent/agents/te_db_agent/subagent.py
+++ chemdx_agent/agents/te_db_agent/subagent.py
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-from pydantic_ai import Agent, RunContext
-from chemdx_agent.schema import AgentState, Result
-
-
-system_prompt = "You are the agent of the thermoelectric materials database. you have access to the database of thermoelectric materials at different composition ratios and doping. you also have access to their seebeck_coefficient(μV/K),electrical_conductivity(S/m),thermal_conductivity(W/mK),power_factor(W/mK2) and ZT data at differnet temperatures (K)."
-
-general_agent = Agent(
-=======
 from typing import List
 from pydantic_ai import Agent, RunContext
 
@@ -25,8 +16,7 @@ Important Context: {context}
 """
 
 
-TE_DB_agent = Agent(
->>>>>>> Stashed changes
+sample_agent = Agent(
     model = "openai:gpt-4o",
     output_type = Result,
     deps_type = AgentState,
@@ -34,64 +24,34 @@ TE_DB_agent = Agent(
         "temperature": 0.0,
         "parallel_tool_calls": False,
     },
-<<<<<<< Updated upstream
-    system_propmpt = system_prompt,
-)
-
-
-# Tool setting
-@general_agent.tool_plain
-def material_property_search(name: str) -> str:
-    """Search the desired property of the material
-    args:
-        name: (str) The name of the material to search
-        property: (str) The property value of the material to search
-    output:
-        (str) The 
-=======
     system_prompt = system_prompt,
 )
 
 # Tool setting
-@TE_DB_agent.tool_plain
-def material_properties_at_temp(args_1: str, args_2: List[str]) -> str:
+@sample_agent.tool_plain
+def sample_function(args_1: str, args_2: List[str]) -> str:
     """Sample function
     args:
-        Name: (str) Material name
-        Temp: (str) Target temperature (K)
+        args_1: (str) The first argument
+        args_2: (List[str]) The second argument
     output:
         (str) The result of the function
->>>>>>> Stashed changes
     """
     return "FEJWOE"
 
 
-<<<<<<< Updated upstream
-
 # call agent function
-async def call_general_agent(ctx: RunContext[AgentState], message2agent: str):
-    """Call general agent to execute the task: general agnet can ~~
-    To call draw agent, numpy array for data is needed.
-=======
-# call agent function
-async def call_TE_DB_agent(ctx: RunContext[AgentState], message2agent: str):
+async def call_sample_agent(ctx: RunContext[AgentState], message2agent: str):
     f"""Call general agent to execute the task: {role}
->>>>>>> Stashed changes
 
     args:
         message2agent: (str) A message to pass to the agent. Since you're talking to another AGENT, you must describe in detail and specifically what you need to do.
     """
-<<<<<<< Updated upstream
-    deps = ctx.deps
-    return await general_agent.run(
-        message2agent, deps=deps
-    )
-=======
     agent_name = "SampleAgent"
     deps = ctx.deps
 
     logger.info(f"[{agent_name}] Message2Agent: {message2agent}")
-    result = await TE_DB_agent.run(
+    result = await sample_agent.run(
         message2agent, deps=deps
     )
     output = result.output
@@ -100,4 +60,3 @@ async def call_TE_DB_agent(ctx: RunContext[AgentState], message2agent: str):
     logger.info(f"[{agent_name}] Result: {output.result}")
 
     return output
->>>>>>> Stashed changes
