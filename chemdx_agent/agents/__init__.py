1ba6f49c5e4d61461b7e923fab826d3f33d58af1
aairehtuele <aairehtuele@gmail.com>
2025-08-21 10:54:29 +0900

    Update __init__.py

--- chemdx_agent/agents/__init__.py
+++ chemdx_agent/agents/__init__.py
@@ -1,12 +1,10 @@
+from .general_agent.subagent import call_general_agent
+from .sample_agent.subagent import call_sample_agent
 from .phosphor_lookup_agent.subagent import call_phosphor_lookup_agent
 from .recommend_agent.subagent import call_recommend_agent
 from .tme_db_agent.subagent import call_database_agent
 from .data_vis_agent.subagent import call_viz_agent
-from .mat_proj_lookup_agent.subagent import call_materials_project_agent 
-from .dft_poscar_agent.subagent import call_dft_poscar_agent
-from .color_trend_agent.subagent import call_color_trend_agent
-from .MatDX_trend_agent.subagent import call_matdx_trend_agent
-from .Phosphor_trend_agent.subagent import call_phosphor_trend_agent
-from .estm_trend_agent.subagent import call_estm_trend_agent
-from .Phosphor_data_research_agent.subagent import call_phosphor_data_research_agent
-from .trend_agent.subagent import call_trend_agent
+from .mat_proj_lookup_agent.subagent import call_mp_agent 
+from .dft_poscar_agent.subagent import call_poscar_agent
+
+
