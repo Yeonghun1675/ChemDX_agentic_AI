b108c72fac5d5f5f511483a341bdfbf7a22cd590
BG Park <bgpark@BGui-MacBookAir.local>
2025-08-21 14:17:32 +0900

    park

--- chemdx_agent/main_agent.py
+++ chemdx_agent/main_agent.py
@@ -31,10 +31,10 @@ main_agent = Agent(
 # connect main agent with subagent
 main_agent.tool(call_database_agent)
 main_agent.tool(call_poscar_agent)
-main_agent.tool(call_recommend_agent)
 main_agent.tool(call_viz_agent)
 main_agent.tool(call_mp_agent)
-main_agent.tool(call_phosphor_lookup_agent)
+main_agent.tool(call_phosphor_data_research_agent)
+main_agent.tool(call_trend_agent)
 
 
 
