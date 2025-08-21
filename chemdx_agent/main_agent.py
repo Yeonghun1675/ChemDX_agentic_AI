6450eb56a216a318d3241b88757cf67bfe50d416
aairehtuele <aairehtuele@gmail.com>
2025-08-21 10:28:31 +0900

    update

--- chemdx_agent/main_agent.py
+++ chemdx_agent/main_agent.py
@@ -1,4 +1,4 @@
-from pydantic_ai import Agent
+
 from pydantic_graph import BaseNode
 from dataclasses import dataclass
 from typing import Optional
@@ -8,7 +8,7 @@ from chemdx_agent.logger import logger
 from chemdx_agent.agents import *
 
 
-system_prompt = "You are the main agent of the ChemDX database."
+system_prompt = "You are the Main Agent of ChemDX Agentic AI. Your role is to efficiently solve complex tasks by coordinating sub-agents rather than handling problems directly. Break down the main task into smaller, well-defined subproblems, and delegate each to the most suitable sub-agent. Always value efficiency: avoid redundant steps and reuse results when possible. Your responsibility is to integrate the sub-agents’ outputs, resolve conflicts if their results differ, and decide the next step. You do not perform the detailed work of solving subproblems; you orchestrate, monitor progress, and ensure the final solution is coherent and complete. You MUST call on several agents to complete the task. If the user asks to generate a POSCAR file you MUST call on the dft poscar agent and the mat proj lookup agent and the databse agent."
 
 tools = []
 
@@ -20,13 +20,19 @@ main_agent = Agent(
         "temperature": 0.0,
         "parallel_tool_calls": False,
     },
-    system_prompt = system_prompt,
+    system_propmpt = system_prompt,
     tools = tools
 )
 
 # connect main agent with subagent
+main_agent.tool(call_database_agent)
+main_agent.tool(call_dft_poscar_agent)
+main_agent.tool(call_recommend_agent)
+main_agent.tool(call_viz_agent)
+main_agent.tool(call_materials_project_agent)
+main_agent.tool(call_phosphor_lookup_agent)
 main_agent.tool(call_sample_agent)
-#main_agent.tool(call_phosphor_lookup_agent)
+main_agent.tool(call_general_agent)
 
 
 async def run_main_agent(message: str, deps: Optional[AgentState] = None):
